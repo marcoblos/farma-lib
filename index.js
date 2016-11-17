4df35a58a515730bd95f5ca37da9daf20472bfd0
marcoblos <marcoblos1234@hotmail.com>
2016-11-17 02:05:27 -0200

    Teste

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-import Components from './components'
+/**
+ * @providesModule farma-lib
+ */
 
-export {
-    Components
-}
-
+export { NavBar } from './playground/components'
+export { ViewContainer } from './playground/components'
+export { FaButton } from './playground/components'
+export { FaSelectModal } from './playground/components'
+export { FaRadioList } from './playground/components'
+export { Input } from './playground/components'
+export { FaText } from './playground/components'
+export { ButtonList } from './playground/components'
+export { Info } from './playground/components'
+export { BoxView } from './playground/components'
+export { NavBarModal } from './playground/components'
+export { FaCamera } from './playground/components'
+export { IconMessage } from './playground/components'
+export { FaImageZoom } from './playground/components'
+export { FaIcon } from './playground/components'
+export { ActionButton } from './playground/components'
